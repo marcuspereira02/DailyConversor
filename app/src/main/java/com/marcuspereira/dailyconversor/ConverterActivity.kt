953a8eefd81f6aa7cdc16b265a56c58a20128f09
Marcus Vinícius de Sá Pereira <makitasjc@outlook.com>
2025-02-22 16:07:43 -0300

    layout do aplicativo

--- app/src/main/java/com/marcuspereira/dailyconversor/ConverterActivity.kt
+++ app/src/main/java/com/marcuspereira/dailyconversor/ConverterActivity.kt
@@ -1,27 +1,20 @@
 package com.marcuspereira.dailyconversor
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ConverterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_converter)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnStart = findViewById<Button>(R.id.btn_start)
-        btnStart.setOnClickListener {
-            val intent = Intent (this, ConverterActivity::class.java)
-            startActivity(intent)
         }
     }
 }
